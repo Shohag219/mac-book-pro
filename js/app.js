9e2e450ea43073ab7892cff35d0782c428106637
Mahmudul Hasan Shohag <mahmudulhasanshohag219@gmail.com>
2021-08-21 07:37:16 +0600

    test

---
+++ js/app.js
@@ -0,0 +1,11 @@
+
+const memoryButton16 = document.getElementById('16GB unified memory').addEventListener('click',function(){
+
+    
+    const memoryCost = document.getElementById('memoryCost');
+    const memory = memoryCost.value;
+    console.log(memory);
+
+
+
+});
